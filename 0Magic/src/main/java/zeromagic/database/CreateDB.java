48a27638a9019966f5118f435cdbf2df16165a95
Matt Nemitz <matt.nemitz@gmail.com>
2016-05-26 16:16:20 -0400

    5-26 now with MAVEN!

---
+++ 0Magic/src/main/java/zeromagic/database/CreateDB.java
@@ -0,0 +1,13 @@
+package zeromagic.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CreateDB 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("main runs");
+	}
+}
